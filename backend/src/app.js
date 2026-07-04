fb326fecebfb1f482e5253bf0e41d80ff83d969b
Sujan Poojary <poojarysujan77@gmail.com>
2026-07-04 22:34:38 +0530

    feat: set up Socket.IO server and configure Express middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,7 +2,35 @@ import express from "express";
 import { createServer } from "node:http";
 
 import { Server } from "socket.io";
-
+import { connectToSocket } from "./controllers/socketManager.js";
 import mongoose from "mongoose";
 
 import cors from "cors";
+const app = express();
+const server = createServer(app);
+const io = connectToSocket(server);
+
+
+app.set("port", (process.env.PORT || 8000))
+app.use(cors());
+app.use(express.json({ limit: "40kb" }));
+app.use(express.urlencoded({ limit: "40kb", extended: true }));
+
+
+
+const start = async () => {
+    app.set("mongo_user")
+    const connectionDb = await mongoose.connect("mongodb+srv://sujanpoojary27:codewithSP@cluster0.bunvszu.mongodb.net/?appName=Cluster0")
+
+    console.log(`MONGO Connected DB HOst: ${connectionDb.connection.host}`)
+    server.listen(app.get("port"), () => {
+        console.log("LISTENING ON PORT 8000")
+    });
+
+
+
+}
+
+
+
+start();

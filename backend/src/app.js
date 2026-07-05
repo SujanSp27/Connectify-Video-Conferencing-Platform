4286c1ed3ca3453ba6ef3a3dace7a6d162746b6c
Sujan Poojary <poojarysujan77@gmail.com>
2026-07-05 11:33:39 +0530

    feat(auth): add user registration and login APIs

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,6 +5,7 @@ import { Server } from "socket.io";
 import { connectToSocket } from "./controllers/socketManager.js";
 import mongoose from "mongoose";
 
+import userRoutes from "./routes/users.routes.js";
 import cors from "cors";
 const app = express();
 const server = createServer(app);
@@ -16,7 +17,7 @@ app.use(cors());
 app.use(express.json({ limit: "40kb" }));
 app.use(express.urlencoded({ limit: "40kb", extended: true }));
 
-
+app.use("/api/v1/users", userRoutes);
 
 const start = async () => {
     app.set("mongo_user")

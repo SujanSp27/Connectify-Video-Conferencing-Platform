80848ad4a288b95d9018fce6a40b786d75204b73
Sujan Poojary <poojarysujan77@gmail.com>
2026-07-11 22:40:09 +0530

    feat: add authentication page with login and signup functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import LandingPage from './pages/landing';
+import Authentication from './pages/authentication';
 function App() {
   return (
     <div className="App">
@@ -8,6 +9,7 @@ function App() {
       <BrowserRouter>
           <Routes>
             <Route path='/' element={<LandingPage />} />
+             <Route path='/auth' element={<Authentication />} />
           </Routes>
     </BrowserRouter>
     </div>

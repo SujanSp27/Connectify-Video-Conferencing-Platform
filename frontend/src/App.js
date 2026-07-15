4678fc18eed79751b88bc6447fc5bca0c4b79998
Sujan Poojary <poojarysujan77@gmail.com>
2026-07-15 22:20:04 +0530

    Implemented useRef for socket and media stream management

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import LandingPage from './pages/landing';
 import Authentication from './pages/authentication';
+import VideoMeetComponent from './pages/VideoMeet';
 import { AuthProvider } from './contexts/AuthContext';
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Routes>
             <Route path='/' element={<LandingPage />} />
              <Route path='/auth' element={<Authentication />} />
+             <Route path='/:url' element={<VideoMeetComponent />} />
           </Routes>
            </AuthProvider>
     </BrowserRouter>
---
+++ frontend/src/pages/VideoMeet.jsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useRef, useState } from 'react'
+import styles from "../styles/videoComponent.module.css";
+const server_url = "http://localhost:8000";
+
+var connections = {};
+
+const peerConfigConnections = {
+    "iceServers": [
+        { "urls": "stun:stun.l.google.com:19302" }
+    ]
+}
+
+export default function VideoMeetComponent() {
+
+    var socketRef = useRef();
+    let socketIdRef = useRef();
+
+    let localVideoref = useRef();
+
+    let [videoAvailable, setVideoAvailable] = useState(true);
+
+    let [audioAvailable, setAudioAvailable] = useState(true);
+
+    let [video, setVideo] = useState([]);
+
+    let [audio, setAudio] = useState();
+
+    let [screen, setScreen] = useState();
+
+    let [showModal, setModal] = useState(true);
+
+    let [screenAvailable, setScreenAvailable] = useState();
+
+    let [messages, setMessages] = useState([])
+
+    let [message, setMessage] = useState("");
+
+    let [newMessages, setNewMessages] = useState(3);
+
+    let [askForUsername, setAskForUsername] = useState(true);
+
+    let [username, setUsername] = useState("");
+
+    const videoRef = useRef([])
+
+    let [videos, setVideos] = useState([])
+ return (
+        <div>
+            <p>VideoMeetComponent</p>
+        </div>
+ )
+}

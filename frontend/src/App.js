c22d9d20789d60e6e1ae68d9d5daed5c832a000c
Sujan Poojary <poojarysujan77@gmail.com>
2026-08-16 21:27:46 +0530

    feat: add authentication HOC

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import LandingPage from './pages/landing';
 import Authentication from './pages/authentication';
 import VideoMeetComponent from './pages/VideoMeet';
+import HomeComponent from './pages/home';
 import { AuthProvider } from './contexts/AuthContext';
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Routes>
             <Route path='/' element={<LandingPage />} />
              <Route path='/auth' element={<Authentication />} />
+             <Route path='/home's element={<HomeComponent />} />
              <Route path='/:url' element={<VideoMeetComponent />} />
           </Routes>
            </AuthProvider>
---
+++ frontend/src/pages/home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import withAuth from '../utils/withAuth'    
+function HomeComponent() {
+    return (
+        <div>HomeComponent</div>
+    )
+}
+
+export default withAuth(HomeComponent)

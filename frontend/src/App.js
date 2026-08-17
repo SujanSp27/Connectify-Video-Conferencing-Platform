14c0ae821be55a1d69ab5947125953729fee0b80
Sujan Poojary <poojarysujan77@gmail.com>
2026-08-17 23:29:39 +0530

    feat: add route for meeting history

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import LandingPage from './pages/landing';
 import Authentication from './pages/authentication';
 import VideoMeetComponent from './pages/VideoMeet';
 import HomeComponent from './pages/home';
+import History from './pages/history';
 import { AuthProvider } from './contexts/AuthContext';
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
             <Route path='/' element={<LandingPage />} />
              <Route path='/auth' element={<Authentication />} />
              <Route path='/home's element={<HomeComponent />} />
+             <Route path='/history' element={<History />} />
              <Route path='/:url' element={<VideoMeetComponent />} />
           </Routes>
            </AuthProvider>
